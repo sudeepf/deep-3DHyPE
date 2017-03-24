One with the working model ToDO: fix the covariance value for all the input

models/hg_graph_builder.py
@@ -8,37 +8,40 @@ import utils.data_prep
 
 class HGgraphBuilder():
 	def __init__(self, FLAG):
-		# first Build Model and define all the place holders
-		# This can go to another file under name model
-		# These parameters can be read from a external config file
-		print ("start build model...")
-		
-		# from string model struct to list model struct
-		steps = map(int, FLAG.structure_string.split('-'))
-		total_dim = np.sum(np.array(steps))
-		
-		self._x = tf.placeholder(tf.float32, [None, FLAG.image_res, FLAG.image_res,
-		                                      FLAG.image_c])
-		self.y = tf.placeholder(tf.float32, [None, FLAG.volume_res,
-		                                     FLAG.volume_res, FLAG.num_joints *
-		                                     total_dim])
-		
-		# If I ever write a handle for accuracy computation in TF
-		self.gt = tf.placeholder(tf.float32, [None, FLAG.num_joints,
-		                                      3])
-		
-		self.output = hg.stacked_hourglass(steps, 'stacked_hourglass')(self._x)
-		
-		# Defining Loss with root mean square error
-		self.loss = tf.reduce_mean(tf.square(self.output - self.y))
-		
-		self.optimizer = tf.train.RMSPropOptimizer(FLAG.learning_rate)
-		
-		self.train_step = tf.Variable(0, name='global_step', trainable=False)
-		
-		self.train_rmsprop = self.optimizer.minimize(self.loss, self.train_step)
-		
-		utils.add_summary.add_all(self._x, self.y, self.output, self.loss)
+		with tf.variable_scope('model_graph'):
+			# first Build Model and define all the place holders
+			# This can go to another file under name model
+			# These parameters can be read from a external config file
+			print ("start build model...")
+			
+			# from string model struct to list model struct
+			steps = map(int, FLAG.structure_string.split('-'))
+			total_dim = np.sum(np.array(steps))
+			
+			self._x = tf.placeholder(tf.float32, [None, FLAG.image_res, FLAG.image_res,
+			                                      FLAG.image_c])
+			self.y = tf.placeholder(tf.float32, [None, FLAG.volume_res,
+			                                     FLAG.volume_res, FLAG.num_joints *
+			                                     total_dim])
+			
+			# If I ever write a handle for accuracy computation in TF
+			self.gt = tf.placeholder(tf.float32, [None, FLAG.num_joints,
+			                                      3])
+			
+			self.output = hg.stacked_hourglass(steps, 'stacked_hourglass')(self._x)
+			
+			
+			
+			# Defining Loss with root mean square error
+			self.loss = tf.reduce_mean(tf.square(self.output - self.y))
+			
+			self.optimizer = tf.train.RMSPropOptimizer(FLAG.learning_rate)
+			
+			self.train_step = tf.Variable(0, name='global_step', trainable=False)
+			
+			self.train_rmsprop = self.optimizer.minimize(self.loss, self.train_step)
+			
+			utils.add_summary.add_all(self._x, self.y, self.output, self.loss)
 
 
 class HGgraphBuilder_MultiGPU():
@@ -92,7 +95,37 @@ class HGgraphBuilder_MultiGPU():
 							y = utils.data_prep.volumize_vec_gpu(tensor_x, tensor_y,
 							                                     tensor_z, FLAG)
 							
-							label = utils.data_prep.prepare_output_gpu(y, steps, FLAG)
+							label = []
+							yo = y
+							label.append(tf.reshape(yo, [FLAG.batch_size,
+							                                            FLAG.volume_res,
+							                                            FLAG.volume_res,
+							                                            FLAG.volume_res*
+							                                            FLAG.num_joints]))
+							yo1 = tf.nn.avg_pool3d(yo, [1, 2, 2, 2, 1], [1, 2, 2, 2, 1],
+								                 'VALID', name='to32')
+							label.append(tf.reshape(yo1								, [FLAG.batch_size,
+							                                            FLAG.volume_res/2,
+							                                            FLAG.volume_res/2,
+							                                            FLAG.volume_res*
+							                                            FLAG.num_joints/2]))
+							yo1 = tf.nn.avg_pool3d(yo1, [1, 2, 2, 2, 1], [1, 2, 2, 2, 1],
+							                       'VALID', name='to16')
+							label.append(tf.reshape(yo1 ,[FLAG.batch_size,
+							                                            FLAG.volume_res/4,
+							                                            FLAG.volume_res/4,
+							                                            FLAG.volume_res*
+							                                            FLAG.num_joints/4]))
+							yo1 = tf.nn.avg_pool3d(yo1, [1, 2, 2, 2, 1], [1, 2, 2, 2, 1],
+							                       'VALID', name='to8')
+							label.append(tf.reshape(
+								yo1, [FLAG.batch_size,
+							                                            FLAG.volume_res/8,
+							                                            FLAG.volume_res/8,
+							                                            FLAG.volume_res*
+							                                            FLAG.num_joints/8]))
+							
+							#label = utils.data_prep.prepare_output_gpu(y, steps, FLAG)
 							self.label.append(label)
 							
 							# If I ever write a handle for accuracy computation in TF
@@ -129,13 +162,13 @@ class HGgraphBuilder_MultiGPU():
 			
 			self.loss /= len(map(int, FLAG.gpu_string.split('-')))
 			
-			utils.add_summary.add_all_joints(
-				utils.eval_utils.get_precision_MultiGPU(self.output, self.label, [],
-				                                        FLAG),
-				FLAG)
+			#utils.add_summary.add_all_joints(
+			#	utils.eval_utils.get_precision_MultiGPU(self.output, self.label, [],
+		  #		                                        FLAG),
+			#	FLAG)
 			
-			utils.add_summary.add_all(self._x[0], self.label[0], self.output[0],
-			                          self.loss)
+			#utils.add_summary.add_all(self._x[0], self.label[0], self.output[0],
+			#                          self.loss)
 			# We must calculate the mean of each gradient. Note that this is the
 			# synchronization point across all towers.
 			grads = self.average_gradients(tower_grads)
@@ -173,10 +206,21 @@ class HGgraphBuilder_MultiGPU():
 		output = hg.stacked_hourglass(steps, 'stacked_hourglass')(_x)
 		
 		# Defining Loss with root mean square error
-		loss = tf.reduce_mean(tf.square(output - y))
+		# loss = tf.reduce_mean(tf.square(output - y))
+		for i in xrange(len(y)):
+			for j in xrange(4):
+				loss = tf.reduce_mean(tf.square(output[j][i] - y[3-j]))
+				# Calculate the total loss for the current tower.
+				tf.add_to_collection('losses', loss)
+		
+		# Add final loss
+		loss = tf.reduce_mean(tf.square(output[-1][0] - y[0]))
+		tf.add_to_collection('losses', loss)
+		#loss = tf.reduce_mean(tf.square(output[0][0] - y[3 - 0]))
+		
 		
 		# Calculate the total loss for the current tower.
-		tf.add_to_collection('losses', loss)
+		#tf.add_to_collection('losses', loss)
 		# Calculate the total loss for the current tower.
 		total_loss = tf.add_n(tf.get_collection('losses', scope), name='total_loss')
 		

models/stacked_supervised_hourglass.py
@@ -6,6 +6,7 @@ class stacked_hourglass():
 		self.nb_stack = len(steps)
 		self.steps = steps
 		self.name = name
+		self.module_supervisions = [[],[],[],[],[]]
 	
 	def __call__(self, x):
 		with tf.name_scope(self.name) as scope:
@@ -26,32 +27,36 @@ class stacked_hourglass():
 			out_ = [None] * self.nb_stack
 			sum_ = [None] * self.nb_stack
 			with tf.variable_scope('_hourglass_0_with_supervision') as sc:
-				hg[0] = self._hourglass(r3, 4, 256, '_hourglass')
+				hg[0] = self._hourglass(r3, 4, 64*14, '_hourglass', 0)
 				ll[0] = self._conv_bn_relu(hg[0], 256, name='conv_1')
 				ll_[0] = self._conv(ll[0], 256, 1, 1, 'VALID', 'll')
-				out[0] = self._conv(ll[0], 14 * self.steps[0], 1, 1, 'VALID', 'out')
+				out[0] = self._conv(ll[0], 14 * 64, 1, 1, 'VALID', 'out')
 				out_[0] = self._conv(out[0], 256, 1, 1, 'VALID', 'out_')
 				sum_[0] = tf.add_n([ll_[0], out_[0], r3])
 			for i in range(1, self.nb_stack - 1):
 				with tf.variable_scope(
 							'_hourglass_' + str(i) + '_with_supervision') as sc:
-					hg[i] = self._hourglass(sum_[i - 1], 4, 256, '_hourglass')
+					hg[i] = self._hourglass(sum_[i - 1], 4, 64*14, '_hourglass', i)
 					ll[i] = self._conv_bn_relu(hg[i], 256, name='conv_1')
 					ll_[i] = self._conv(ll[i], 256, 1, 1, 'VALID', 'll')
-					out[i] = self._conv(ll[i], 14 * self.steps[i], 1, 1, 'VALID', 'out')
+					out[i] = self._conv(ll[i], 14 * 64, 1, 1, 'VALID', 'out')
 					out_[i] = self._conv(out[i], 256, 1, 1, 'VALID', 'out_')
 					sum_[i] = tf.add_n([ll_[i], out_[i], sum_[i - 1]])
 			with tf.variable_scope(
 						'_hourglass_' + str(self.nb_stack - 1) + '_with_supervision') as sc:
-				hg[self.nb_stack - 1] = self._hourglass(sum_[self.nb_stack - 2], 4, 256,
-				                                        '_hourglass')
+				hg[self.nb_stack - 1] = self._hourglass(sum_[self.nb_stack - 2], 4,
+				                                        64*14,
+				                                        '_hourglass' , self.nb_stack-1)
 				ll[self.nb_stack - 1] = self._conv_bn_relu(hg[self.nb_stack - 1], 256,
 				                                           name='conv_1')
-				out[self.nb_stack - 1] = self._conv(ll[self.nb_stack - 1],
+				self.module_supervisions[-1].append(self._conv(ll[self.nb_stack - 1],
 				                                    14 * self.steps[self.nb_stack - 1],
 				                                    1, 1,
-				                                    'VALID', 'out')
-			return tf.concat(out, axis=3)
+				                                    'VALID', 'out'))
+				
+			#return tf.concat(out, axis=3)
+			#return self.module_supervisions
+			return self.module_supervisions
 	
 	def _conv(self, inputs, nb_filter, kernel_size=1, strides=1, pad='VALID',
 	          name='conv'):
@@ -113,21 +118,39 @@ class stacked_hourglass():
 			_skip_layer = self._skip_layer(inputs, nb_filter_out)
 			return tf.add(_skip_layer, _conv_block)
 	
-	def _hourglass(self, inputs, n, nb_filter_res, name='_hourglass'):
+	def _hourglass(self, inputs, n, nb_filter_res, name='_hourglass', rank_=0):
 		with tf.variable_scope(name) as scope:
-			# Upper branch
-			up1 = self._residual_block(inputs, nb_filter_res, 'up1')
-			# Lower branch
-			pool = tf.contrib.layers.max_pool2d(inputs, [2, 2], [2, 2], 'VALID')
-			low1 = self._residual_block(pool, nb_filter_res, 'low1')
+			
 			if n > 1:
-				low2 = self._hourglass(low1, n - 1, nb_filter_res, 'low2')
+				# Upper branch
+				up1 = self._residual_block(inputs, nb_filter_res, 'up1')
+				# Lower branch
+				pool = tf.contrib.layers.max_pool2d(inputs, [2, 2], [2, 2], 'VALID')
+				low1 = self._residual_block(pool, nb_filter_res/2, 'low1')
+				low2 = self._hourglass(low1, n - 1, nb_filter_res/2, 'low2', rank_)
+				low2 = tf.add(low2,up1,'merged1')
 			else:
-				low2 = self._residual_block(low1, nb_filter_res, 'low2')
+				low1 = self._residual_block(inputs, nb_filter_res, 'low1_1')
+				low2 = self._residual_block(low1, nb_filter_res, 'low2_1')
+			
 			low3 = self._residual_block(low2, nb_filter_res, 'low3')
-			low4 = tf.image.resize_nearest_neighbor(low3, tf.shape(low3)[1:3] * 2,
-			                                        name='upsampling')
+			
+			lower1 = self._residual_block(low3, nb_filter_res, 'lower1')
+			
+			self.module_supervisions[n-1].append(lower1)
+			
 			if n < 4:
-				return tf.add(up1, low4, name='merge')
+				lower2 = tf.image.resize_nearest_neighbor(lower1, tf.shape(lower1)[1:3] * 2,
+			                                        name='upsampling_2')
+				lower2 = self._residual_block(lower2, nb_filter_res*2, 'lower2')
+				low4 = tf.image.resize_nearest_neighbor(low3, tf.shape(low3)[1:3] * 2,
+				                                        name='upsampling_1')
+				low4 = self._residual_block(low4, nb_filter_res*2, 'low4')
 			else:
-				return self._residual_block(tf.add(up1, low4), nb_filter_res, 'low4')
+				lower2 = self._residual_block(lower1, nb_filter_res, 'lower2')
+				low4 = self._residual_block(low3, nb_filter_res , 'low4')
+			
+			
+				
+			if n < 5:
+				return tf.add(lower2, low4, name='merge')

train.py
@@ -12,7 +12,6 @@ from scipy import misc
 import sys
 import numpy as np
 import time
-import hourglass_TF.src.stacked_hourglass as hg
 import matplotlib.pyplot as plt
 
 #Get all the custom helper util headers
@@ -21,7 +20,7 @@ import utils.add_summary
 import utils.train_utils
 import utils.eval_utils
 import utils.get_flags
-import include.hg_graph_builder
+import models.hg_graph_builder
 
 
 #Read up and set up all the flag variables
@@ -39,19 +38,24 @@ def main(_):
 
   with tf.Graph().as_default():
 		
-		#builder = include.hg_graph_builder.HGgraphBuilder(FLAG)
+		builder = models.hg_graph_builder.HGgraphBuilder_MultiGPU(FLAG)
 
-		builder = include.hg_graph_builder.HGgraphBuilder_MultiGPU(FLAG)
+		#builder = models.hg_graph_builder.HGgraphBuilder(FLAG)
 		print ("build finished, There it stands, tall and strong...")
 		
+		graph_def = tf.get_default_graph().as_graph_def()
+		graphpb_txt = str(graph_def)
+		with open('graphpb.txt', 'w') as f:
+			f.write(graphpb_txt)
 		
+		#lol = lol2
 		config = tf.ConfigProto()
 		config.gpu_options.allow_growth = True
 	
 		saver = tf.train.Saver()
 	
-		#with tf.Session(config=config) as sess:
-		with tf.Session() as sess:
+		with tf.Session(config=config) as sess:
+		#with tf.Session() as sess:
 			merged = tf.summary.merge_all()
 	
 			# All the variable initialiezed in MoFoking RunTime
@@ -78,8 +82,8 @@ def main(_):
 				      (ckpt.model_checkpoint_path, global_step))
 			else:
 				print('No checkpoint file found')
-				#tf.global_variables_initializer().run()
-				saver.restore(sess, "./tensor_record//tmp/model1-2-4-64.ckpt")
+				tf.global_variables_initializer().run()
+				#saver.restore(sess, "./tensor_record//tmp/model1-2-4-64.ckpt")
 				print('model Initialized...')
 			
 	