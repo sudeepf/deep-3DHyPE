1d2e6eef46f5425a2f18b2b938b79b75bb2ceed2
Sudeep Fadadu <fadadusudeep93@gmail.com>
2017-03-25 13:08:13 -0400

    Fixed testing.py file and some file format fixes

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 nice -n 10 python testing.py \
 --structure_string=1-2-4-64 \
 --data_split_string_train=S1 \
---data_split_string_test=S9 \
+--data_split_string_test=S1 \
 --batch_size=2 \
 --joint_prob_max=3 \
 --sigma=1 \
