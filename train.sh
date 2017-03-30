375d459998ffbcab971a93a9b31c413209607ed4
Your Name <you@example.com>
2017-03-30 16:42:11 -0400

    stashing changes

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 nice -n 10 python train.py \
 --structure_string=64-64-64-64 \
---data_split_string_train=S1 \
+--data_split_string_train=S1-S0-S5-S6-S7-S8 \
 --data_split_string_test=S1 \
---batch_size=1 \
+--batch_size=4 \
 --joint_prob_max=3 \
 --sigma=0.5 \
 --gpu_string=0 \
