f521f4af27063c9f7bb9a31a4a78637d99554591
Your Name <you@example.com>
2017-04-01 13:07:35 -0400

    commit chages for the working case of 2d data

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 nice -n 10 python train_2d.py \
 --structure_string=64-64 \
---data_split_string_train=S1 \
+--data_split_string_train=S1-S0-S5-S6-S7-S8 \
 --data_split_string_test=S1 \
 --batch_size=4 \
 --joint_prob_max=1 \
---sigma=0.5 \
+--sigma=0.75 \
 --gpu_string=0-1 \
---learning_rate=2e-4 \
---train_2d=True \
---dataset_dir=./Dataset_2d/
+--learning_rate=5e-5 \
+--train_2d=False \
+--dataset_dir=./Dataset/
 
