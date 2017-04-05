c3a15dfba04c022ec012aa2db33f41e4872cbe25
Sudeep Fadadu <fadadusudeep93@gmail.com>
2017-04-05 01:33:49 -0400

    removed soft max and the normalization

--- train.sh
+++ train.sh
@@ -2,10 +2,10 @@ nice -n 10 python train_2d.py \
 --structure_string=64-64 \
 --data_split_string_train=S1 \
 --data_split_string_test=S1 \
---batch_size=1 \
+--batch_size=4 \
 --joint_prob_max=6 \
---sigma=0.7 \
---gpu_string=0 \
+--sigma=0.75 \
+--gpu_string=0-1 \
 --learning_rate=2e-4 \
 --train_2d=True \
 --dataset_dir=./Dataset_2d/
